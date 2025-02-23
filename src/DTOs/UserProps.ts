0d1af38a96e503ea185af2f07a40302e52e262fa
J0aoCunha <jvjunger20123@gmail.com>
2025-02-23 19:32:33 -0300

    feat: criando a entidade de usuário e seu gateway

---
+++ src/DTOs/UserProps.ts
@@ -0,0 +1,10 @@
+export type userProps = {
+    id: string,
+    name: string,
+    email: string,
+    money: number,
+    cpf_cnpj: string,
+    endereco: string,
+    password: string, 
+    user_type: 'user' | 'lojista'
+}
---
+++ src/domains/User/entity/user.ts
@@ -0,0 +1,70 @@
+import { userProps } from '../../../DTOs/UserProps';
+export class User {
+
+    private constructor(private props: userProps){}
+
+    public static create(name: string, email: string, cpf_cnpj: string, endereco: string, password:string){
+        return new User({
+            id: crypto.randomUUID(),
+            name,
+            email,
+            cpf_cnpj,
+            endereco,
+            money: 0,
+            password,
+            user_type: 'user'
+        });
+    }
+
+    public static with(props: userProps){
+        return new User(props)
+    }
+
+    public get id(){
+        return this.props.id;
+    }
+
+    public get name(){
+        return this.props.name;
+    }
+
+    public get email(){
+        return this.props.email;
+    }
+
+    public get money(){
+        return this.props.money;
+    }
+
+    public get cpf_cnpj(){
+        return this.props.cpf_cnpj;
+    }
+
+    public get endereco(){
+        return this.props.endereco;
+    }
+
+    public get password(){
+        return this.props.password;
+    }
+
+    public get userType(){
+        return this.props.user_type;
+    }
+
+    public addMoney(value: number){
+
+        if(value <= 0){
+            throw new Error('Invalid value');
+        }
+            
+        this.props.money += value;
+    }
+
+    public removeMoney(value: number){
+        if(this.props.user_type === 'lojista'){
+            throw new Error('Lojista cannot transfer money');
+        }
+        this.props.money -= value;
+    }
+}
